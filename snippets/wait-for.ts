c29e65cef96ba835f524f3967905bbfae8f8c4bb
Kirk Strobeck <kirk@strobeck.com>
2022-02-02 11:25:15 -0800

    Create wait-for.ts

---
+++ snippets/wait-for.ts
@@ -0,0 +1,23 @@
+import { debugLog } from 'root/src/utils/logger';
+
+const waitFor = (fn: Function, note?: string) =>
+  new Promise((resolve) => {
+    const result = fn();
+    if (result) resolve(result);
+
+    let attempts = 0;
+
+    const interval = window.setInterval(() => {
+      debugLog('`waitFor` interval run for:', note);
+
+      const intervalResult = fn();
+      attempts += 1;
+
+      if (attempts > 500 || intervalResult) {
+        window.clearInterval(interval);
+        resolve(intervalResult);
+      }
+    }, 100);
+  });
+
+export default waitFor;
